25ea5f1c1645377ffc83346904848403781c1ae1
WeareEDT <104486332+weareedt@users.noreply.github.com>
2024-10-29 16:00:33 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ConsolePage } from './pages/ConsolePage';
 import './App.scss';
 
-
 function App() {
   return (
+    <Router>
     <div className="App">
       <div className="content-wrapper">
+      <Routes>
         {/*<Visualizer />*/}  
-        <ConsolePage />
+        <Route path="/console" element={<ConsolePage/>} />
+        </Routes>
       </div>
-    </div>
+      </div>
+    </Router>
   );
 }
 
