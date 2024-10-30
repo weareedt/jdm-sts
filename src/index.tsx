e95fc7aa605a5d99cfc37aa45667bb2b7a0615a5
nabil-01-utm <nabil-01@graduate.utm.my>
2024-10-30 18:53:51 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ConsolePage } from './pages/ConsolePage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ConsolePage />
+    <App />
   </React.StrictMode>
 );
