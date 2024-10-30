06bccb43a1ff5ad8d530e2b8557166d14898d03d
nabil-01-utm <nabil-01@graduate.utm.my>
2024-10-30 18:49:46 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ConsolePage } from './pages/ConsolePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ConsolePage />
   </React.StrictMode>
 );
